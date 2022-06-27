b3e0141fcf54e676f2ea27dc17a93c37bed4a7a2
Gd Ocha <gdochadipa@gmail.com>
2022-06-28 03:55:29 +0800

    stage-4

--- app/user/controller.js
+++ app/user/controller.js
@@ -18,5 +18,31 @@ module.exports = {
         } catch (err) {
             res.status(500).json({ message: err.message || `Internal server error` })
         }
-    }
+    },
+     editProfile: async(req, res) => {
+        try {
+            let request = req.body;
+
+            if(req.user._id != request._id){
+                return res.status(403).json({ message: "User tidak ditemukan", status: false })
+            }
+
+            await Users.findOneAndUpdate({_id:req.user.id},{
+                username: request.username,
+                email: request.email,
+                name: request.name
+
+            });
+
+            let user = await Users.findOne({ _id: req.user.id });
+
+            res.status(200).json({ data: user, status: true });
+
+        } catch (err) {
+            res.status(500).json({ message: err.message || `Internal server error` })
+        }
+     },
+     editPhotoProfile: async(req, res) => {
+        
+     }
 }
